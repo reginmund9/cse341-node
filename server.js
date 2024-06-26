e952fafe7deca12f2a67c48ac0170959807db240
reginmund9 <moronitaca@gmail.com>
2024-06-25 23:38:10 -0600

    w2-render

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const mongodb = require ('./data/database');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
-app.use('/', require ('./routes'));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next(); 
+  });
+
+app.use('/', require ('./routes')); 
 
 
 mongodb.initDb((err) => {
@@ -16,3 +27,5 @@ mongodb.initDb((err) => {
         app.listen(port, () => { console.log(`Database is initialized and Node server is running on port ${port}`)});
     }
 });
+
+
